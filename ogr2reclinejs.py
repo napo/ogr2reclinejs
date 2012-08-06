d445c821903911625253d4a9d5b63ab9ab26a7e0
Maurizio Napolitano <napo@fbk.eu>
2012-08-06 10:01:54 +0200

    improved the command line

--- ogr2reclinejs.py
+++ ogr2reclinejs.py
@@ -17,22 +17,33 @@ class OGR2Reclinejs():
     outfiles = []
     fields = {}
     geojson = 'geojson'
-    
-    def __init__(self,infile,filetype='ESRI Shapefile'): 
+    formatfound = ''
+    def __init__(self,infile,verbose=False): 
+        supportedformats = []
         self.sr_wgs84 = osr.SpatialReference()
         self.sr_wgs84.ImportFromEPSG(4326)
-        self.filetype=filetype
-        driver = ogr.GetDriverByName(self.filetype)
-        self.datasource = driver.Open(infile)
-        if self.datasource is None:
-            message = 'Could not open ' + infile
-            message += "\n"
-            message += "Supported formats:\n"
-            for g in range(0,ogr.GetDriverCount()):
-                message += ogr.GetDriver(g).GetName() + "\n"
-            raise Exception, message
-        for idx in range(0,self.datasource.GetLayerCount()):
-            layer = self.datasource.GetLayer(idx)
+        
+        driver = None
+        for g in range(0,ogr.GetDriverCount()):
+            supportedformats.append(ogr.GetDriver(g).GetName())
+            driver= ogr.GetDriver(g)
+            datasource = driver.Open(infile)
+            if datasource != None:
+                self.datasource = datasource
+                self.formatfound = ogr.GetDriver(g).GetName()
+                break
+        
+        if datasource is None:
+            message = 'Could not find the driver for %s' % infile
+            if verbose:
+                message += "\n"
+                message += "List of supported drivers:"
+                for sf in supportedformats:
+                    message += "- %s\n" % sf
+            raise Exception,message
+            
+        for idx in range(0,datasource.GetLayerCount()):
+            layer = datasource.GetLayer(idx)
             sr = layer.GetSpatialRef() 
             if sr is None:
                 raise Exception, 'Projection not present'
@@ -40,7 +51,17 @@ class OGR2Reclinejs():
             for s in layer.schema:
                 self.fields[s.GetName()] = s.GetTypeName()
             layer.ResetReading()
-
+            
+    def info(self):
+        data = {}
+        if self.datasource != None:
+            data['format'] = self.formatfound
+            data["num_layer"] =  self.datasource.GetLayerCount()
+            for i in range(0,self.datasource.GetLayerCount()):
+                nl = "layer_" + i
+                data[nl] = self.datasource.GetLayer(i).GetName()
+        return data
+        
     def metadata(self):
         mfields = self.fields
         mfields['geometry'] = self.geojson
@@ -77,35 +98,36 @@ class OGR2Reclinejs():
                     writer.writerow(values)
                     feature = layer.GetNextFeature()
                 layer.ResetReading()
-        except EOFError as e:
+        except OSError as e:
             print e
 
 def main():
     usage = "usage: %prog [options]"
     parser = OptionParser(usage)  
     parser.add_option("-i", "--input", action="store", dest="input", help="input file")
+    parser.add_option("-o","--ouput",action="store",dest="output",help="output file\nif not specified is used the layer name with the csv extension")
     parser.add_option("-f", "--filetype", action="store", dest="filetype", help="input file type - default is ESRI Shapefile",default='ESRI Shapefile')
-    parser.add_option("-d", "--destinatiodir", action="store", dest="destdir", help="output directory - default is same directory of input file",default=None)
+    parser.add_option("-d", "--destinatiodir", action="store", dest="destdir", help="output directory - default is current directory",default=None)
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", help="verbose output",default=False)
+    parser.add_option("-I","--info",action="store",dest="info",help="show info file (tips: use the ogrinfo)",default=False)
     (options,args) = parser.parse_args()
     if options.input is None:
         parser.print_help()
     else:
-        ogr2reclinejs = OGR2Reclinejs(options.input,options.filetype)
+        v = False
+        if options.verbose:
+            v = True
+        ogr2reclinejs = OGR2Reclinejs(options.input,v)
         ogr2reclinejs.conversion(options.destdir)
-        ff = ogr2reclinejs.outputfiles()
-        m = "file"
-        if len(ff) > 1:
-            m = "%i files" % (len(ff))
-        m += "generated\n"
-        for f in ff:
-            m += "-%s\n" % (f)
-        print m
-        print "\n"
-        print "Fields: name and type"
-        metadata = ogr2reclinejs.metadata()
-        for m in metadata:
-            print "%s => %s" % (m,metadata[m])
-        print "----"
+        if v:
+            for f in ogr2reclinejs.outputfiles():
+                
+            print "\n"
+            print "Fields: name and type"
+            metadata = ogr2reclinejs.metadata()
+            for m in metadata:
+                print "%s => %s" % (m,metadata[m])
+                print "----"
         print "File %s generated" % f
 if __name__ == "__main__":
     main()
