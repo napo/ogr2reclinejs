e5e1771c4c7cf6a29d00171b91f906c2675d4f4f
Maurizio Napolitano <napo@staifo>
2012-09-04 12:38:32 +0200

    Changelog:  * patch made by Michele Mostarda - SpazioDati  * improved the verbose mode  * added the skip function for the cell size to big for reclinejswq

--- ogr2reclinejs.py
+++ ogr2reclinejs.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Aug  5 21:22:51 2012
-@author: Maurizio Napolitano
+@author: Maurizio Napolitano <napo@fbk.eu>
+@contributors: Michele Mostarda
 MIT License
 """
-import os,csv
+import os,csv,chardet
 
 #search the right version of the
 try:
@@ -16,7 +17,7 @@ from optparse import OptionParser
 MAX_CSV_FIELD_SIZE = 131072
 
 class OGR2Reclinejs():
-	version = '1.0b'
+	version = '1.1'
 	sr_wgs84 = None
 	datasource = None
 	outfiles = []
@@ -25,14 +26,19 @@ class OGR2Reclinejs():
 	X = 'LON'
 	Y = 'LAT'
 	formatfound = ''
-	def __init__(self,infile,verbose=False):
+	verbose = False
+	resize = False
+	encoding = 'utf-8'
+	def __init__(self,infile,verbose=False,resize=False,encoding='utf-8'):
 		'''
-				create a object with the source file (infile = geodata source)
-				'''
+		create a object with the source file (infile = geodata source)
+		'''
+		self.verbose = verbose
+		self.resize = resize
+		self.encoding = encoding
 		supportedformats = []
 		self.sr_wgs84 = osr.SpatialReference()
 		self.sr_wgs84.ImportFromEPSG(4326)
-
 		driver = None
 		for g in range(0,ogr.GetDriverCount()):
 			supportedformats.append(ogr.GetDriver(g).GetName())
@@ -61,8 +67,10 @@ class OGR2Reclinejs():
 			fields = {}
 			self.geomtypes.append(layer.GetGeomType())
 			for s in layer.schema:
-				print 'FOUND SCHEMA FIELD', s.GetName()
-				fields[s.GetName().decode('latin-1')] = s.GetTypeName()
+				if verbose:
+					print 'FOUND SCHEMA FIELD', s.GetName()
+				nf = s.GetName()
+				fields[nf.decode(encoding)] = s.GetTypeName()
 			self.layers_fields.append(fields)
 			layer.ResetReading()
 
@@ -75,10 +83,11 @@ class OGR2Reclinejs():
 				'''
 		data = {}
 		if self.datasource != None:
+			#data['encoding'] = self.source_encoding
 			data['format'] = self.formatfound
 			data["num_layer"] =  self.datasource.GetLayerCount()
 			for i in range(0,self.datasource.GetLayerCount()):
-				nl = "layer_" + i
+				nl = "layer_" + str(i)
 				data[nl] = self.datasource.GetLayer(i).GetName()
 		return data
 
@@ -101,8 +110,8 @@ class OGR2Reclinejs():
 
 	def conversion(self,destdir=None):
 		'''For each layer present in the source file create a csv file with the geometry field.
-				If the geometry type is a point, use the fields LAT and LON, otherwise create a GeoJSON string
-				'''
+		If the geometry type is a point, use the fields LAT and LON, otherwise create a GeoJSON string
+		'''
 		try:
 			for idx in range(0,self.datasource.GetLayerCount()):
 				layer = self.datasource.GetLayer(idx)
@@ -114,8 +123,9 @@ class OGR2Reclinejs():
 				writer = csv.writer(ofile, dialect='excel')
 				head = []
 				for f in self.layers_fields[idx]:
-					field = f.decode('latin-1').encode('utf-8')
-					print 'F:[%s]' % field
+					field = f.decode(self.encoding).encode('utf-8')
+					if self.verbose:
+						print 'F:[%s]' % field
 					head.append(field)
 				if self.geomtypes[idx] == 1:
 					head.append(self.X)
@@ -130,7 +140,7 @@ class OGR2Reclinejs():
 					for fname in self.layers_fields[idx]:
 						v = feature.items().get(fname)
 						#if v is None: print 'Missing value for fname', fname
-						values.append( v.decode('latin-1').encode('utf-8') if type(v) == str else v )
+						values.append( v.decode(self.encoding).encode('utf-8') if type(v) == str else v )
 					geometry = feature.GetGeometryRef()
 					if (sr_source != self.sr_wgs84):
 						ct = osr.CoordinateTransformation(sr_source, self.sr_wgs84)
@@ -142,10 +152,13 @@ class OGR2Reclinejs():
 						values.append(y)
 					else:
 						geometry_json = geometry.ExportToJson().encode('utf-8')
-						if len(geometry_json) <= MAX_CSV_FIELD_SIZE:
-							values.append(geometry_json)
+						if self.resize:
+							if len(geometry_json) <= MAX_CSV_FIELD_SIZE:
+								values.append(geometry_json)
+							else:
+								print 'Skipping geometry line'
 						else:
-							print 'Skipping geometry line'
+							values.append(geometry_jsoN)
 					writer.writerow(values)
 					feature = layer.GetNextFeature()
 				layer.ResetReading()
@@ -159,7 +172,9 @@ def main():
 	parser = OptionParser(usage)
 	#FIXME parser.add_option("-o","--ouput",action="store",dest="output",help="output file\nif not specified is used the layer name with the csv extension")
 	parser.add_option("-d", "--destinatiodir", action="store", dest="destdir", help="output directory - default is current directory, the name is the layer name with the .csv suffix",default=None)
+	parser.add_option("-e","--encoding",action="store",dest="encoding",help="Charachter encoding source data - default=utf-8",default="utf-8")
 	parser.add_option("-v", "--verbose", action="store_true", dest="verbose", help="verbose output",default=False)
+	parser.add_option("-r","--resize",action="store_true",dest="resize",help="resize or skip the geometry size. Be carefull: this CHANGE the data!")
 	parser.add_option("-I","--info",action="store_true",dest="info",help="show info file, you need to use also the -i parameter (tip: with the command ogrinfo you obtain more informations)",default=False)
 
 	(options,args) = parser.parse_args()
@@ -169,15 +184,20 @@ def main():
 	else:
 		inputfile = args[0]
 		v = False
+		r = False
 		if options.verbose:
 			v = True
-		ogr2reclinejs = OGR2Reclinejs(inputfile,v)
+		if options.resize:
+			r = True
+		ogr2reclinejs = OGR2Reclinejs(inputfile,v,r,options.encoding)
 		ogr2reclinejs.conversion(options.destdir)
 		if v:
 			idx = 0
+			info = ogr2reclinejs.info()
 			metadata = ogr2reclinejs.metadata()
 			for f in ogr2reclinejs.outputfiles():
 				print "file %s" % f
+				#print "charachter encoding = %s" % info['encoding']
 				print "\tfields for %s:" % f
 				mt = metadata[idx]
 				for m in mt:
